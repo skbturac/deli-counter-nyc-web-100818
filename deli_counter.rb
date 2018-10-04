da7ab489f2a04348370ab44598923068d4e946f0
Suheyl Turac <skbsturac@live.com>
2018-10-04 04:45:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@
 katz_deli = []
 
  def line(katz_deli)
+   
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
@@ -19,6 +20,7 @@ end
 end
 
 def now_serving(katz_deli)
+  
   if katz_deli.length == 0
   puts "There is nobody waiting to be served!"
 else
